22a3b291685b04acf9b65109eed2f8ba5f7b0569
pchan1401-ICIL <pchan1401@hanmail.net>
2016-06-11 13:09:54 +0900

    Initial commit

---
+++ app/src/main/java/icil/com/androidpermission/MainActivity.java
@@ -0,0 +1,118 @@
+package icil.com.androidpermission;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private Activity mainActivity = this;
+
+    private static final int REQUEST_MICROPHONE = 3;
+    private static final int REQUEST_EXTERNAL_STORAGE = 2;
+    private static final int REQUEST_CAMERA = 1;
+
+    private TextView resultText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button cameraBtn = (Button)findViewById(R.id.cameraBtn);
+        Button storageBtn = (Button)findViewById(R.id.storageBtn);
+        Button micBtn = (Button)findViewById(R.id.micBtn);
+
+        resultText = (TextView)findViewById(R.id.resultText);
+
+        cameraBtn.setOnClickListener(buttonClickListener);
+        storageBtn.setOnClickListener(buttonClickListener);
+        micBtn.setOnClickListener(buttonClickListener);
+    }
+
+    private View.OnClickListener buttonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            int id = v.getId();
+            switch (id) {
+                case R.id.cameraBtn:
+                    int permissionCamera = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA);
+                    if(permissionCamera == PackageManager.PERMISSION_DENIED) {
+                        ActivityCompat.requestPermissions(mainActivity, new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA);
+                    } else {
+                        resultText.setText("camera permission authorized");
+                    }
+                    break;
+                case R.id.storageBtn:
+                    int permissionReadStorage = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE);
+                    int permissionWriteStorage = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                    if(permissionReadStorage == PackageManager.PERMISSION_DENIED || permissionWriteStorage == PackageManager.PERMISSION_DENIED) {
+                        ActivityCompat.requestPermissions(mainActivity, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_EXTERNAL_STORAGE);
+                    } else {
+                        resultText.setText("read/write storage permission authorized");
+                    }
+                    break;
+                case R.id.micBtn:
+                    int permissionAudio = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.RECORD_AUDIO);
+                    if(permissionAudio == PackageManager.PERMISSION_DENIED) {
+                        ActivityCompat.requestPermissions(mainActivity, new String[]{Manifest.permission.RECORD_AUDIO}, REQUEST_MICROPHONE);
+                    } else {
+                        resultText.setText("recording audio permission authorized");
+                    }
+                    break;
+            }
+        }
+    };
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_CAMERA:
+                for (int i = 0; i < permissions.length; i++) {
+                    String permission = permissions[i];
+                    int grantResult = grantResults[i];
+                    if (permission.equals(Manifest.permission.CAMERA)) {
+                        if(grantResult == PackageManager.PERMISSION_GRANTED) {
+                            resultText.setText("camera permission authorized");
+                        } else {
+                            resultText.setText("camera permission denied");
+                        }
+                    }
+                }
+                break;
+            case REQUEST_EXTERNAL_STORAGE:
+                for (int i = 0; i < permissions.length; i++) {
+                    String permission = permissions[i];
+                    int grantResult = grantResults[i];
+                    if (permission.equals(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                        if(grantResult == PackageManager.PERMISSION_GRANTED) {
+                            resultText.setText("read/write storage permission authorized");
+                        } else {
+                            resultText.setText("read/write storage permission denied");
+                        }
+                    }
+                }
+                break;
+            case REQUEST_MICROPHONE:
+                for (int i = 0; i < permissions.length; i++) {
+                    String permission = permissions[i];
+                    int grantResult = grantResults[i];
+                    if (permission.equals(Manifest.permission.RECORD_AUDIO)) {
+                        if(grantResult == PackageManager.PERMISSION_GRANTED) {
+                            resultText.setText("recording audio permission authorized");
+                        } else {
+                            resultText.setText("recording audio permission denied");
+                        }
+                    }
+                }
+                break;
+        }
+    }
+}
